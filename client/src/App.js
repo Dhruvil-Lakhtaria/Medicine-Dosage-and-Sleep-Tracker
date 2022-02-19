a6abae81044dbc919e8c2c074bcb342eb448d83b
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2022-02-19 17:29:02 +0530

    main-section with cards | responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import TopSection  from "./components/TopSection";
+import TopSection from "./components/TopSection";
 import Navbar from "./components/Navbar";
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <TopSection />
+      <MainSection />
     </div>
   );
 }
