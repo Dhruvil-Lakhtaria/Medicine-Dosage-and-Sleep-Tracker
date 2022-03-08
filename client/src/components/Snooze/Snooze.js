278b7593882f05f0a5550edbba7d4a3826b9a0e5
RohitTaparia <rohitktaparia@gmail.com>
2022-03-08 13:51:00 +0530

    Added dialog box Tip of the day

--- client/src/components/Snooze/Snooze.js
+++ client/src/components/Snooze/Snooze.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import TopSection from './TopSection';
 import Form from './Form.js'
+import Footer from './Footer.js'
 
 
 const Snooze = () => {
   return (
     <div className='Snooze'>
+        
         <TopSection/>
         <Form />
+        <Footer />
     </div>
   )
 }
