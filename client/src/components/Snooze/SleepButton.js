884a3ec49606bd830c155bac8bf22121b871deb4
RohitTaparia <rohitktaparia@gmail.com>
2022-03-12 16:15:09 +0530

    Awake button modified

--- client/src/components/Snooze/SleepButton.js
+++ client/src/components/Snooze/SleepButton.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 //import Date from './Date'
 const SleepButton = () => {
-    function showTime() {
+    const [idx, incrementIdx] = useState(0);
+    const [isSleeping, setIsSleeping] = useState(false);
+    const [sleepingTimes, addSleepingTime] = useState([]);
+    const [wakingTimes, addWakeTime] = useState([]);
+    const [SleepTime, addSleepTime] = useState([]);
+    function handleClick() {
+        setIsSleeping(!isSleeping)
         const date = new Date();
-        const showTime = date.getHours() 
-        + ':' + date.getMinutes() 
-        + ":" + date.getSeconds();
-        alert(showTime);
-      }
-  return (
-    <div className='Sleep_Button'>
-        <div className="Sleep_Text">
-            <datetime />
-            Please click this button when you start sleeping.
+        if (!isSleeping) {
+            alert(`Sleeping @ ${date}`)
+            addSleepingTime([...sleepingTimes, date]);
+        }
+        else {
+            alert(`Woke @ ${date}`)
+            addWakeTime([...wakingTimes, date]);
+            addSleepTime([...SleepTime, wakingTimes[idx] - sleepingTimes[idx]])
+            incrementIdx(idx + 1);
+        }
+
+    }
+    return (
+        <div className='Sleep_Button'>
+            <div className="Sleep_Text">
+                <datetime />
+                Please click this button when you start or stop sleeping.
+            </div>
+            <center>
+                <Button id="buttonSleep" variant="outlined" onClick={handleClick}>
+                    {isSleeping ? "Sleeping" : "Awake"}
+                </Button>
+            </center>
         </div>
-        <center>
-        <Button id="buttonSleep" variant="outlined" onClick={showTime}>
-                I am Sleeping!!
-        </Button>
-        </center>
-    </div>
-  )
+    )
 }
 
 export default SleepButton
