9b834d7e542ad8a00c74570df3284e690b4d416d
RohitTaparia <rohitktaparia@gmail.com>
2022-03-12 21:18:30 +0530

    Removed unnecessary files

--- client/src/components/Snooze/Snooze.js
+++ client/src/components/Snooze/Snooze.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import TopSection from './TopSection';
-import Form from './Form.js'
+
 import SleepButton from './SleepButton';
 import SnoozeFooter from './SnoozeFooter.js'
-import Date from './Date.js'
+
 
 const Snooze = () => {
   return (
