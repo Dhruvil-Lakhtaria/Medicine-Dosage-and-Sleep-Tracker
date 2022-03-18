b6821911c6e81fd57e1d26b761146c6c965840db
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2022-03-18 13:19:27 +0530

    feat:local user authetication

--- client/src/components/RegisterPage/LoginForm.js
+++ client/src/components/RegisterPage/LoginForm.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+  const [signupDetails, addDetails] = useState([]);
+  const [userName, updateUserName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [password, updatePassword] = useState("");
+  const [loggedEmail, updateLoggedEmail] = useState("");
+  const [loggedPassword, updateLoggedPassword] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newDetails = {
+      userName: userName,
+      email: email,
+      password: password,
+    };
+    addDetails([...signupDetails, newDetails]);
+    updateUserName("");
+    updateEmail("");
+    updatePassword("");
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signupDetails.forEach((signUp) => {
+      if (loggedEmail === signUp.email && loggedPassword === signUp.password) {
+        alert("Registered User");
+      }
+      updateLoggedEmail("");
+      updateLoggedPassword("");
+    });
+  };
   return (
     <div className="login-wrapper">
       <div className="main">
@@ -9,6 +37,8 @@ const LoginForm = () => {
           <form>
             <label htmlFor="chk">Sign up</label>
             <input
+              value={userName}
+              onChange={(e) => updateUserName(e.target.value)}
               type="text"
               className="login-input"
               name="txt"
@@ -16,6 +46,8 @@ const LoginForm = () => {
               required=""
             />
             <input
+              value={email}
+              onChange={(e) => updateEmail(e.target.value)}
               type="email"
               className="login-input"
               name="email"
@@ -23,13 +55,17 @@ const LoginForm = () => {
               required=""
             />
             <input
+              value={password}
+              onChange={(e) => updatePassword(e.target.value)}
               className="login-input"
               type="password"
               name="pswd"
               placeholder="Password"
               required=""
             />
-            <button className="signup-btn">Sign up</button>
+            <button className="signup-btn" onClick={handleClick}>
+              Sign up
+            </button>
           </form>
         </div>
 
@@ -37,20 +73,24 @@ const LoginForm = () => {
           <form>
             <label htmlFor="chk">Login</label>
             <input
+              value={loggedEmail}
+              onChange={(e) => updateLoggedEmail(e.target.value)}
               type="email"
               className="login-input"
               name="email"
               placeholder="Email"
-              required=""
             />
             <input
+              value={loggedPassword}
+              onChange={(e) => updateLoggedPassword(e.target.value)}
               type="password"
               name="pswd"
               className="login-input"
               placeholder="Password"
-              required=""
             />
-            <button className="login-btn">Login</button>
+            <button className="login-btn" onClick={handleLogin}>
+              Login
+            </button>
           </form>
         </div>
       </div>
